e9924fefb853293f84ce71327e0b6ebb97b5d7e8
JackChuahe <601825672@qq.com>
2026-05-20 17:38:19 +0800

    feat(telemetry-view): add package skeleton and aiohttp dependency

---
+++ src/alibabacloud/mcp_proxy/telemetry_view/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from alibabacloud.mcp_proxy.telemetry_view.server import run_telemetry_view
+
+__all__ = ["run_telemetry_view"]
---
+++ src/alibabacloud/mcp_proxy/telemetry_view/server.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+
+async def run_telemetry_view(port: int, no_open: bool) -> None:
+    raise NotImplementedError
